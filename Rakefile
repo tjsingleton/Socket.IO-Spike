8cefcd6ff08e7ee0d4cad54ad3632dec4311fde8
TJ Singleton <tjsingleton@vantagestreet.com>
2011-11-09 13:58:21 -0500

    getting started w\ Cap for deployment
    * Remove node_modules - needs to be compiled
    * Add Cap for deploy
    * Add Rakefile for building socket

---
+++ Rakefile
@@ -0,0 +1,6 @@
+
+namespace :socketio do
+  task :build do
+    puts system "cd socket.io; npm install socket.io"
+  end
+end
---
+++ config/deploy.rb
@@ -0,0 +1,16 @@
+set :application, "socket-test"
+set :repository,  "git@github.com:tjsingleton/Socket.IO-Spike.git"
+
+set :scm, :git
+
+set :user, "cruncher"
+set :use_sudo, false
+set :port, 22345
+
+ssh_options[:forward_agent] = true    # use our ssh key
+default_run_options[:pty] = true      # Must be set for the password prompt from git to work
+
+role :app, "echo-staging.crunchconnect.com"
+set :deploy_to, "/home/#{user}/socket-test"
+set :deploy_via, :copy
+set :branch, "master"
